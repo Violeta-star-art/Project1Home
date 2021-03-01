81239dcabbb56c628f84b34e2b610d95516b9610
Violeta-star-art <violetateova@gmail.com>
2021-03-01 23:08:23 +0100

    change in price

--- Task1/src/Calculation2.java
+++ Task1/src/Calculation2.java
@@ -1,7 +1,7 @@
 public class Calculation2 {
     public static void main(String[] args) {
         // write your code here
-        int price = 250;
+        int price = 150;
         int discountPercentage = 5;
         float productPriceAfterDiscount = price - price * (discountPercentage / 100.0f);
         System.out.println(productPriceAfterDiscount);
