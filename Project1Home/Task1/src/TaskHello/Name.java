5ea1d2a6608dfbc6cb182efa2e0c609f15e5454d
Violeta-star-art <79420573+Violeta-star-art@users.noreply.github.com>
2021-03-01 21:46:37 +0100

    Add files via upload

---
+++ Project1Home/Task1/src/TaskHello/Name.java
@@ -0,0 +1,15 @@
+package TaskHello;
+
+public class Name{
+
+        public static void main(String[] args) {
+            // write your code here
+
+            System.out.println("Hello New Project Team!!!");
+            System.out.println("This is Test");
+            System.out.println("Hope everithing will be ok");
+
+
+
+        }
+}
