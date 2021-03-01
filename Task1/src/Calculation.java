c850c8f389ea85fb5dd1a15a8060cc765d35c4f0
Violeta-star-art <violetateova@gmail.com>
2021-03-01 21:31:33 +0100

    change in message and calculation

---
+++ Task1/src/Calculation.java
@@ -0,0 +1,12 @@
+public class Calculation {
+
+    public static void main(String[] args) {
+        // write your code here
+        int number = 180;
+        int firstDigit, secondDigit;
+        firstDigit = number / 10;
+        secondDigit = number % 10;
+        int sum = firstDigit + secondDigit;
+        System.out.println(sum);
+    }
+}
